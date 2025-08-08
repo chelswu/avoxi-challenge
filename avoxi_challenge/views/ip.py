5bea9b97aa62f7337dff5765e3b5eb65d9fef74a
Chelsea Wu <chelseawu@chelseas-mbp.lan>
2025-08-08 15:38:49 -0400

    Add in some error handling

--- avoxi_challenge/views/ip.py
+++ avoxi_challenge/views/ip.py
@@ -1,11 +1,15 @@
+import logging
 from django.http import JsonResponse
 from drf_yasg.utils import swagger_auto_schema
+from geoip2.errors import GeoIP2Error, AuthenticationError, InvalidRequestError
 from geoip2.webservice import Client
 from rest_framework import status
 from rest_framework.views import APIView
 
 from django.conf import settings
 from avoxi_challenge.serializers.ip import IPValidationSerializer, IPValidationResponseSerializer
+
+logger = logging.getLogger(__name__)
 
 class IPValidationView(APIView):
     """
@@ -17,15 +21,21 @@ class IPValidationView(APIView):
         responses={
             200: IPValidationResponseSerializer,
             400: "Bad Request",
+            500: "Server Error",
         },
     )
     def get(self, request):
         serializer = IPValidationSerializer(data=request.query_params)
         serializer.is_valid(raise_exception=True)
 
-        geolite_client = Client(settings.GEO_IP_ACCOUNT, settings.GEO_IP_LICENSE, host="geolite.info")
-        geolite_response = geolite_client.country(serializer.validated_data["ip"])
-        iso_code = geolite_response.country.iso_code
+        try:
+            geolite_client = Client(settings.GEO_IP_ACCOUNT, settings.GEO_IP_LICENSE, host="geolite.info")
+            geolite_response = geolite_client.country(serializer.validated_data["ip"])
+            iso_code = geolite_response.country.iso_code
+            # Optional: handle OutOfQueriesError by getting data from database
+        except GeoIP2Error as error:
+            logger.exception(f"Error connecting to GeoIP client: {error}")
+            return JsonResponse({"error": "Error getting country code from IP"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         response_serializer = IPValidationResponseSerializer(data={
             "ip": serializer.validated_data["ip"],
